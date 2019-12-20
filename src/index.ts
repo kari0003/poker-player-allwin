735cabb540c39088173db770f4ce4a9750587981
Balint Mero <balint.mero@codingsans.com>
2019-12-20 14:55:52 +0100

    Update version

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import { Player } from './Player';
 
-const VERSION = 'Sziasztok3.5';
+const VERSION = 'Sziasztok3.6';
 
 const app = express();
 const player = new Player();
