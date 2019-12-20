7589c17c3cb055df2d21fdc3ac49cfe2503cc30c
Balint Mero <balint.mero@codingsans.com>
2019-12-20 11:03:40 +0100

    Increase bet

--- src/Player.ts
+++ src/Player.ts
@@ -1,15 +1,36 @@
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    if (gameState.current_buy_in > 500) {
-      betCallback(0)
+    const myPlayer = gameState.players[gameState.in_action];
+    if (gameState.current_buy_in > 600) {
+      betCallback(0);
     } else {
-      betCallback(gameState.current_buy_in - gameState.players[gameState.in_action]['bet'] + 1);
+      betCallback(gameState.current_buy_in - myPlayer["bet"] + 4);
     }
   }
 
-  public showdown(gameState: any): void {
+  public showdown(gameState: any): void {}
 
+  private areMyCardsOk(myPlayer: any): boolean {
+    const myCards = myPlayer["hole_cards"];
+    const value = 0;
+    // switch(myCards)
+    return true;
   }
-};
+
+  private cardToValue(card: any): number {
+    switch (card.rank) {
+      case "A":
+        return 20;
+      case "K":
+        return 15;
+      case "Q":
+        return 14;
+      case "J":
+        return 12;
+      default:
+        return parseInt(card.rank, 10);
+    }
+  }
+}
 
 export default Player;
