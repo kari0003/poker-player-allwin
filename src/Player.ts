abaddce6044a0a52c98463af91abdbad38e936fb
Balint Mero <balint.mero@codingsans.com>
2019-12-20 10:56:18 +0100

    Fold after 500

--- src/Player.ts
+++ src/Player.ts
@@ -1,6 +1,10 @@
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    betCallback(gameState.current_buy_in - gameState.players[gameState.in_action]['bet'] + 1);
+    if (gameState.current_buy_in > 500) {
+      betCallback(0)
+    } else {
+      betCallback(gameState.current_buy_in - gameState.players[gameState.in_action]['bet'] + 1);
+    }
   }
 
   public showdown(gameState: any): void {
