157070d989dc5afd06906aa5152deb6f05c60556
Balint Mero <balint.mero@codingsans.com>
2019-12-20 13:02:16 +0100

    Updates

--- src/Player.ts
+++ src/Player.ts
@@ -1,58 +1,72 @@
 export class Player {
-  public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    const myPlayer = gameState.players[gameState.in_action];
-    // if (gameState.current_buy_in > 600) {
+  public betRequest(any: any, betCallback: (bet: number) => void): void {
+    const myPlayer = any.players[any.in_action];
+    // if (any.current_buy_in > 600) {
     //   betCallback(0);
     // } else {
-      const bet = myPlayer ? myPlayer["bet"] : 0;
-      betCallback(
-        gameState.current_buy_in - bet + this.calculateRaise(gameState)
-      );
+    const bet = myPlayer ? myPlayer["bet"] : 0;
+    betCallback(any.current_buy_in - bet + this.calculateRaise(any));
     // }
   }
 
-  public showdown(gameState: any): void {}
+  public showdown(any: any): void {}
 
-  public calculateRaise(gameState: any): number {
-    let raise = gameState.minimum_raise;
-    // const myPlayer = gameState.players[gameState.in_action];
-    const myBet = 20;
+  public calculateRaise(any: any): number {
+    let raise = any.minimum_raise;
+    const myPlayer = any.players[any.in_action];
+    const myBet = this.calculateBet(myPlayer);
     if (!raise || raise < myBet) {
       raise = myBet;
+    }
+    if (raise > myPlayer.stack) {
+      raise = myPlayer.stack;
     }
     return raise;
   }
 
-  // private calculateBet(myPlayer: any): number {
-  //   const myCards = myPlayer["hole_cards"];
-  //   const communityCards = myPlayer["community_cards"];
-  //   const communityCardsValues = communityCards.map(this.cardToValue);
-  //   const value = this.cardToValue(myCards[0]) + this.cardToValue(myCards[1]);
-  //   const diff = Math.abs(
-  //     this.cardToValue(myCards[0]) - this.cardToValue(myCards[1])
-  //   );
+  private calculateBet(myPlayer: any): number {
+    const communityCards = myPlayer["community_cards"] || [];
+    const communityCardsValues: number[] = communityCards.map(this.cardToValue);
+    const communityCardsValue = communityCardsValues.reduce(
+      (val, curr) => val + curr,
+      0
+    );
+    const communityCardsDiff = communityCardsValues.reduce(
+      (val, curr) => val - curr,
+      0
+    );
 
-  //   if (diff <= 1) {
-  //     return 50;
-  //   }
+    const myCards = myPlayer["hole_cards"];
+    const value = this.cardToValue(myCards[0]) + this.cardToValue(myCards[1]);
+    const diff = Math.abs(
+      this.cardToValue(myCards[0]) - this.cardToValue(myCards[1])
+    );
 
-  //   return 20;
-  // }
+    const allValue = value + communityCardsValue;
+    const alldiff = Math.abs(diff - communityCardsDiff);
 
-  // private cardToValue(card: any): number {
-  //   switch (card.rank) {
-  //     case "A":
-  //       return 20;
-  //     case "K":
-  //       return 15;
-  //     case "Q":
-  //       return 14;
-  //     case "J":
-  //       return 12;
-  //     default:
-  //       return parseInt(card.rank, 10);
-  //   }
-  // }
+    if (diff <= 1) {
+      return 50;
+    }
+    // if (value > )
+
+    return 1;
+  }
+
+  private cardToValue(card: any): number {
+    switch (card.rank) {
+      case "A":
+        return 20;
+      case "K":
+        return 15;
+      case "Q":
+        return 14;
+      case "J":
+        return 12;
+      default:
+        return parseInt(card.rank, 10);
+    }
+  }
 }
 
 export default Player;
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import { Player } from './Player';
 
-const VERSION = "Sziasztok2.0";
+const VERSION = "Sziasztok2.1";
 
 const app = express();
 const player = new Player();
